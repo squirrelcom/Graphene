c77e1451815451d956dccda8cfae23fa5a644c86
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2021-06-08 12:34:15 +0100

    Update and rename open_in_graphene.sh to open_in_squirrelnavigator.sh

--- open_in_squirrelnavigator.sh
+++ open_in_squirrelnavigator.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 url=$(echo $line | sed 's#.*\(https*://\)#\1#')
 
 # if [[ "$line" =~ \(pager\)$ ]]; then
-#     bash $DIR/graphene $engine $url
+#     bash $DIR/squirrelnavigator $engine $url
 # else
 #     open $url
 # fi
